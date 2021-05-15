79f262b60d5072d692d45989dbc6a47b67d0cf4c
liao820 <2869799543@qq.com>
2021-05-15 13:23:56 +0800

    删除所有项目文件

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-
-def index(request):
-    print("请耐心等待")
-    return render(request,'index.html')
-    # return HttpResponse("Hello World")
